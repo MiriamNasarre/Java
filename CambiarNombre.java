ec8fd1df119abcdca2a5cffd13305aebfa375b05
MiriamNasarre <miriamnasarre@hotmail.com>
2020-10-26 09:17:18 +0100

    Add files via upload

--- CambiarNombre.java
+++ CambiarNombre.java
@@ -10,7 +10,7 @@ public class CambiarNombre {
 			for(int i=0;i<listaficheros.length;i++) {
 					String [] cmd= {"ren",(char)34+ruta+"\\"+listaficheros[i].getName()+(char)34,(char)34+listaficheros[i].getName().substring(0,listaficheros[i].getName().length()-3)+(char)34}; 
 					System.out.println(cmd[0]+" "+cmd[1]+" "+cmd[2]);
-					if(i==listaficheros.length)
+					if(i==listaficheros.length-1)
 						System.out.println("Se han renombrado "+(i+1)+" ficheros en "+ruta+".");
 					
 					
